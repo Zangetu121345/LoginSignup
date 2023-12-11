70efb24feba3c224e632186deed534988d609d70
Pankaj.Papnai <pankaj.papnai@anarock.com>
2023-12-11 11:20:21 +0530

    database connection issue resolved

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const mysql = require("mysql2");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const connect = require("./configs/db");
+const {connection} = require("./configs/db");
 const port = process.env.PORT || 3232;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// app.listen(port, async function () {
-//   connect();
-// });
+app.listen(port, async function () {
+  connection.connect((err)=>{
+    if(err){
+      throw err
+    }
+    console.log("Connected to database");
+  });
+});
--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,19 +1,14 @@
 const mysql = require("mysql2");
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_ROOT,
+  host: process.env.HOST,
+  user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
+  port:process.env.DATABASE_PORT
 });
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
-  }
-  console.log("Connected to MySQL");
-});
+module.exports = {
+  connection : connection
+}
 
-// Perform your database operations here...
-
-connection.end();
